ce6a4b593e0545730f6b157da93f3a2205e99b42
randolj <r.recio@ufl.edu>
2023-09-16 19:38:53 -0400

    Added an image to swap button, needed to add PIL to resize

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
+import ttkbootstrap as ttk
+from PIL import Image, ImageTk
 
 button_status = 0
 
@@ -7,10 +9,10 @@ button_status = 0
 def convert():
     input = entry_int.get()
     if button_status == 0:
-        km_output = input * 1.61  # 1 mile = 1.61 km
+        km_output = round(input * 1.61, 3)  # 1 mile = 1.61 km
         output_string.set(km_output)
     if button_status == 1:
-        mi_output = input / 1.61
+        mi_output = round(input / 1.61, 3)
         output_string.set(mi_output)
 
 def swap():
@@ -30,7 +32,7 @@ def title_creator():
 
 
 # window
-window = tk.Tk()  # Use Tk() instead of ttk.Window
+window = ttk.Window(themename='darkly')  # Use Tk() instead of ttk.Window
 window.title('Demo')
 window.geometry("350x150")
 
@@ -49,7 +51,15 @@ button.pack(side='left')
 input_frame.pack(pady=10)
 
 swap_frame = ttk.Frame(master=window)
-button2 = ttk.Button(master=swap_frame, text='Swap', command=swap)
+
+# Load and resize the image for the "Swap" button
+swap_image = Image.open("swaparrows.png")
+# Resize the image to fit the button
+swap_image = swap_image.resize((20, 20))
+swap_image = ImageTk.PhotoImage(swap_image)
+
+button2 = ttk.Button(master=swap_frame, image=swap_image, command=swap)
+button2.image = swap_image
 button2.pack()
 swap_frame.pack()
 
